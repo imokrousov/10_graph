16a2801a932fad76b9a73072c5017a4cbaf1b581
Ilya <mokill44@gmail.com>
2023-04-28 15:44:48 +0300

    init

---
+++ Makefile
@@ -0,0 +1,5 @@
+first: main.cpp hello.cpp
+	g++ -o hello hello.cpp main.cpp
+
+second: 
+	echo "Haha$(\n)"
---
+++ graph.h
@@ -0,0 +1,24 @@
+#include <vector>
+
+using namespace std;
+
+
+struct Edge{
+	int from,to;
+	int weight;
+};
+
+struct Graph{
+	int ** matrix;
+	vector<Edge> edges;
+	int e_count, v_count;
+};
+
+Edge make_e (int _from, int _to, int _weight = 0 );
+Graph make_g (int ** m, int n);
+Graph make_g (const vector<Edge>  & es);
+Graph make_g (const vector<pair<int,int>>  & es);
+void print_edge (const Edge & e);
+void print_graph(const Graph & g, bool is_matrix = true);
+
+
